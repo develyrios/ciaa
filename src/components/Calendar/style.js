219f119f00e1bbbe98faa32ee9b1acfbff5a4b60
Bev Feitosa <bevely1407@gmail.com>
2023-04-30 09:35:01 -0300

    <feat>:adiciona o texto da página e refatora alguns códigos relacionados a fonte

--- src/components/Calendar/style.js
+++ src/components/Calendar/style.js
@@ -31,7 +31,7 @@ export const Container = styled.div`
 
     max-width: 1440px;
 
-    padding: 32px 16px;
+    padding: 32px 16px 0 16px;
 
     @media (max-width: 1200px) {
         flex-direction: column;
